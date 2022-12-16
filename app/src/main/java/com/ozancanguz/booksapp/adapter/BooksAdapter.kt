4d82b7ff9e0fe228cb20a36b8921b94927c45df1
Ozan <ozancangu24@gmail.com>
2022-12-16 21:41:21 +0300

    -books adapter created

---
+++ app/src/main/java/com/ozancanguz/booksapp/adapter/BooksAdapter.kt
@@ -0,0 +1,44 @@
+package com.ozancanguz.booksapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ozancanguz.booksapp.R
+import com.ozancanguz.booksapp.data.model.Book
+import com.ozancanguz.booksapp.data.model.Result
+import com.ozancanguz.booksapp.utils.Constants.Companion.loadImage
+import kotlinx.android.synthetic.main.book_row_layout.view.*
+
+
+class BooksAdapter:RecyclerView.Adapter<BooksAdapter.BookViewHolder>(){
+
+    var bookList= emptyList<Result>()
+
+    fun setData(newData: Book){
+        this.bookList=newData.result
+    }
+
+    class BookViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+        val inflater=LayoutInflater.from(parent.context)
+        val view=inflater.inflate(R.layout.book_row_layout,parent,false)
+        return  BookViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+       var currentBook=bookList[position]
+        holder.itemView.books_img.loadImage(currentBook.image)
+        holder.itemView.books_title.text=currentBook.title
+        holder.itemView.books_price.text=currentBook.fiyat
+    }
+
+    override fun getItemCount(): Int {
+        return bookList.size
+    }
+
+
+}
