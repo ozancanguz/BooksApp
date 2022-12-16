afa102175a56d87ba6f1e3e6be7562c8ef2568a2
Ozan <ozancangu24@gmail.com>
2022-12-16 22:37:57 +0300

    -books dao created

---
+++ app/src/main/java/com/ozancanguz/booksapp/data/db/BookDao.kt
@@ -0,0 +1,25 @@
+package com.ozancanguz.booksapp.data.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ozancanguz.booksapp.data.db.entities.BookEntity
+import com.ozancanguz.booksapp.data.model.Book
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BookDao {
+
+
+    // read from db
+    @Query("select *from book_table order by id asc")
+    fun readAllBoooks(): Flow<List<Book>>
+
+    // insert database
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBook(bookEntity: BookEntity)
+
+
+
+}
