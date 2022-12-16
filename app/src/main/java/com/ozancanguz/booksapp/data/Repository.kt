5adc2dbfc563a40121f9007b8a813bafd90b7a94
Ozan <ozancangu24@gmail.com>
2022-12-16 21:19:06 +0300

    -repository created and injected

--- app/src/main/java/com/ozancanguz/booksapp/data/Repository.kt
+++ app/src/main/java/com/ozancanguz/booksapp/data/Repository.kt
@@ -1,4 +1,13 @@
 package com.ozancanguz.booksapp.data
 
-class Repository {
+import com.ozancanguz.booksapp.data.remotedatasource.RemoteDataSource
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
+
+@ViewModelScoped
+class Repository@Inject constructor(remoteDataSource: RemoteDataSource){
+
+
+    val remote=remoteDataSource
+
 }
