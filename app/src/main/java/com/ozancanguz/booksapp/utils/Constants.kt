fb6342009544881bcdf821e02345f6ea21d8ca60
Ozan <ozancangu24@gmail.com>
2022-12-16 21:37:13 +0300

    -glide image load fun created

--- app/src/main/java/com/ozancanguz/booksapp/utils/Constants.kt
+++ app/src/main/java/com/ozancanguz/booksapp/utils/Constants.kt
@@ -1,7 +1,24 @@
 package com.ozancanguz.booksapp.utils
 
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.ozancanguz.booksapp.R
+
 class Constants {
     companion object{
         const val BASE_URL="https://api.collectapi.com/book/"
+
+        fun ImageView.loadImage(uri: String?) {
+            val options = RequestOptions()
+
+                .error(R.mipmap.ic_launcher)
+            Glide.with(context)
+                .setDefaultRequestOptions(options)
+                .load(uri)
+                .into(this)
+
+        }
+
     }
 }
