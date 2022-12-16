683fa648793ec6be89f56903ae0ad94a89cefa6e
Ozan <ozancangu24@gmail.com>
2022-12-16 21:32:25 +0300

    -data displayed in logcat

--- app/src/main/java/com/ozancanguz/booksapp/ui/fragments/booklist/BookListFragment.kt
+++ app/src/main/java/com/ozancanguz/booksapp/ui/fragments/booklist/BookListFragment.kt
@@ -1,18 +1,24 @@
 package com.ozancanguz.booksapp.ui.fragments.booklist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.ozancanguz.booksapp.R
 import com.ozancanguz.booksapp.databinding.FragmentBookListBinding
+import com.ozancanguz.booksapp.viewmodels.BookViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class BookListFragment : Fragment() {
     private var _binding: FragmentBookListBinding? = null
 
     private val binding get() = _binding!!
+    private val bookViewModel:BookViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,8 +27,16 @@ class BookListFragment : Fragment() {
         _binding = FragmentBookListBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        observeLiveData()
 
         return view
+    }
+
+    private fun observeLiveData() {
+        bookViewModel.requestSafeCallAllBooks()
+        bookViewModel.booksList.observe(viewLifecycleOwner, Observer {
+            Log.d("listfragment","" +it.result)
+        })
     }
 
 
