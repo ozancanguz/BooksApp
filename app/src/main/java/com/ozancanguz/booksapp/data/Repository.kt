47d5d0055b3d7c2cac24a688f725e49f678e1b64
Ozan <ozancangu24@gmail.com>
2022-12-16 22:48:14 +0300

    -repository injected with local data source

--- app/src/main/java/com/ozancanguz/booksapp/data/Repository.kt
+++ app/src/main/java/com/ozancanguz/booksapp/data/Repository.kt
@@ -1,13 +1,17 @@
 package com.ozancanguz.booksapp.data
 
+import com.ozancanguz.booksapp.data.localdatasource.LocalDataSource
 import com.ozancanguz.booksapp.data.remotedatasource.RemoteDataSource
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
-class Repository@Inject constructor(remoteDataSource: RemoteDataSource){
+class Repository@Inject constructor(remoteDataSource: RemoteDataSource,
+
+                                    localDataSource: LocalDataSource){
 
 
     val remote=remoteDataSource
+    val local=localDataSource
 
 }
