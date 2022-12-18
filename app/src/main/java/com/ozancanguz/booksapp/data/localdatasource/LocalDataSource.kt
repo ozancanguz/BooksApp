45a24ae6c7bc488b29373f767f56ead5e9f65ba9
Ozan <ozancangu24@gmail.com>
2022-12-18 13:56:35 +0300

    -local data source funs  created

--- app/src/main/java/com/ozancanguz/booksapp/data/localdatasource/LocalDataSource.kt
+++ app/src/main/java/com/ozancanguz/booksapp/data/localdatasource/LocalDataSource.kt
@@ -2,6 +2,7 @@ package com.ozancanguz.booksapp.data.localdatasource
 
 import com.ozancanguz.booksapp.data.db.BookDao
 import com.ozancanguz.booksapp.data.db.entities.BookEntity
+import com.ozancanguz.booksapp.data.db.entities.FavoritesEntity
 import com.ozancanguz.booksapp.data.model.Book
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -17,6 +18,16 @@ class LocalDataSource@Inject constructor(private val bookDao: BookDao) {
 
     suspend fun insertBook(bookEntity: BookEntity){
         return bookDao.insertBook(bookEntity)
+    }
+
+
+    // get and list fav news
+    fun readFavoriteNews():Flow<List<FavoritesEntity>>{
+        return  bookDao.readFavoriteNews()
+    }
+
+    suspend fun insertFavorites(favoritesEntity: FavoritesEntity){
+        return bookDao.insertFavorites(favoritesEntity)
     }
 
 
