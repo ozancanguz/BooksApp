d178b6b82ce761471ba7a16cf39b8e4eba9fcfa6
Ozan <ozancangu24@gmail.com>
2022-12-18 13:52:55 +0300

    -favorites entity created

---
+++ app/src/main/java/com/ozancanguz/booksapp/data/db/entities/FavoritesEntity.kt
@@ -0,0 +1,21 @@
+package com.ozancanguz.booksapp.data.db.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.ozancanguz.booksapp.data.model.Result
+
+
+@Entity(tableName = "favorites_table")
+class FavoritesEntity(
+
+    @PrimaryKey(autoGenerate = true)
+    var id:Int=0,
+    var result:Result
+
+){
+
+
+
+
+
+}
