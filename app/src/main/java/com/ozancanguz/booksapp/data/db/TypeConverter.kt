0252e844d7ff34bfd160de97f190ddb1dcb10873
Ozan <ozancangu24@gmail.com>
2022-12-18 13:54:02 +0300

    -type converted modified

--- app/src/main/java/com/ozancanguz/booksapp/data/db/TypeConverter.kt
+++ app/src/main/java/com/ozancanguz/booksapp/data/db/TypeConverter.kt
@@ -4,6 +4,7 @@ import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.ozancanguz.booksapp.data.model.Book
+import com.ozancanguz.booksapp.data.model.Result
 
 class TypeConverter {
 
@@ -20,5 +21,18 @@ class TypeConverter {
             val listType = object : TypeToken<Book>() {}.type
             return gson.fromJson(data, listType)
         }
+
+    @TypeConverter
+    fun resultToString(result: com.ozancanguz.booksapp.data.model.Result):String{
+        return gson.toJson(result)
+    }
+
+    @TypeConverter
+    fun stringToResult(data:String):com.ozancanguz.booksapp.data.model.Result{
+        val listType=object : TypeToken <Result>(){
+        }.type
+        return gson.fromJson(data,listType)
+    }
+
 
 }
