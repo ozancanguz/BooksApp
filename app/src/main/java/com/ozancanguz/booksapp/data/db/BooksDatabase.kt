873c95002795f3de98edbc0133ba745de59cbb77
Ozan <ozancangu24@gmail.com>
2022-12-18 13:55:53 +0300

    -books db modified for favorites entity

--- app/src/main/java/com/ozancanguz/booksapp/data/db/BooksDatabase.kt
+++ app/src/main/java/com/ozancanguz/booksapp/data/db/BooksDatabase.kt
@@ -4,9 +4,10 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.ozancanguz.booksapp.data.db.entities.BookEntity
+import com.ozancanguz.booksapp.data.db.entities.FavoritesEntity
 
 @Database(
-    entities = [BookEntity::class],
+    entities = [BookEntity::class,FavoritesEntity::class],
     version = 1,
     exportSchema = false,
 )
