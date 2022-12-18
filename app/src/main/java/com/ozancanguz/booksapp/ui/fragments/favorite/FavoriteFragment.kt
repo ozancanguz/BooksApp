af56d2ec59028334106d45d24e45d0a6ab06857d
Ozan <ozancangu24@gmail.com>
2022-12-18 14:38:43 +0300

    -fav books displayed in fav screen

--- app/src/main/java/com/ozancanguz/booksapp/ui/fragments/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/ozancanguz/booksapp/ui/fragments/favorite/FavoriteFragment.kt
@@ -3,15 +3,24 @@ package com.ozancanguz.booksapp.ui.fragments.favorite
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ozancanguz.booksapp.R
+import com.ozancanguz.booksapp.adapter.FavoritesAdapter
 
 import com.ozancanguz.booksapp.databinding.FragmentFavoriteBinding
+import com.ozancanguz.booksapp.viewmodels.BookViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class FavoriteFragment : Fragment() {
     private var _binding: FragmentFavoriteBinding? = null
 
     private val binding get() = _binding!!
+    private val favoritesAdapter=FavoritesAdapter()
+    private val bookViewModel:BookViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,10 +28,28 @@ class FavoriteFragment : Fragment() {
         _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        // set up rv
+           setupRv()
+
+        // update ui
+        updateUI()
 
 
 
         return view
+    }
+    private fun setupRv() {
+        binding.favoriteFragmentRV.layoutManager=LinearLayoutManager(requireContext())
+        binding.favoriteFragmentRV.adapter=favoritesAdapter
+
+    }
+
+    private fun updateUI() {
+         bookViewModel.favoriteBooksList.observe(viewLifecycleOwner, Observer {
+             favoritesAdapter.setFavData(it)
+         })
+
+
     }
 
 
